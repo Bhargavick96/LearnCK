be9348eab40657643467d48be2a17b04bc3ce995
Bhargavick96 <bhargavi.chippagirikaranam@gmail.com>
2021-12-16 18:41:54 +0530

    updated firstfile

--- firstfile.py
+++ firstfile.py
@@ -5,3 +5,6 @@ print(greeting + ' ' + name)
 welcome = 'Hello'
 name = input("Please Enter your name- ")
 print(welcome + ' ' + name)
+
+sliptstrings = "You\nare\nso\nBeautiful\n"
+print(sliptstrings)
